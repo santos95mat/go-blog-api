5ebd49c75e0e0b7ec0f8dd86815c18333b812702
santos95mat <santos95.mat@gmail.com>
2023-09-20 10:54:53 -0300

    feat: create main project package

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	_ "github.com/santos95mat/go-api/docs"
+	"github.com/santos95mat/go-api/src/initializers"
+	"github.com/santos95mat/go-api/src/routes"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectDB()
+}
+
+// @title Tag Service API
+// @version 1.0
+// @description A Tag Service API in Go using Gin
+
+// @host localhost:3000
+// @BasePath /
+func main() {
+	routes.Run()
+}
